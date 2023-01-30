9b3e3daff735cd375afd4f86bae14f7c4c41dda9
Name is Raju <75732731+VORUGANTIPRAVEEN@users.noreply.github.com>
2023-01-30 11:54:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,8 @@ export default function App() {
       <h1>Fetching the data from github api using class Component</h1>
       <Apps />
     <hr />
+          <h1>Using the usestate and useEffect </h1>
      <UserList />
-    <hr />
-      <h1>Using the usestate and useEffect </h1>
-    <Techzone />
-    <hr />
-       <One /> 
     <hr />
       <h1>Using recoil atoms Counting the number of Character in the String </h1>
        <CharacterCounter />
